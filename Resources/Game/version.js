c1a81da950ed4cccee087849efd356255538ad16
Agustin Riondini <105334088+agustinriondini@users.noreply.github.com>
2024-08-25 04:05:43 +0000

    update game version

--- Resources/Game/version.js
+++ Resources/Game/version.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     var actualver = $('#updateVer').text();
     var checkChannel = $('#channel').text();
-    var lastUpdate = "v1.2.2 build86(30062024)";
+    var lastUpdate = "v1.2.3 build88(25082024)";
     var releaseChannel = "Stable";
 
     if (actualver !== lastUpdate) {
